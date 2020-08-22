d5d026dae1eb6dec71fdd9fb33fb5af759a449f0
jesseduarte <jesse__duarte@hotmail.com>
2020-08-22 20:25:42 -0300

    Configurando o arquivo application.properties

--- vendas/src/main/java/io/github/jesseduarte/vendas/MinhaConfiguration.java
+++ vendas/src/main/java/io/github/jesseduarte/vendas/MinhaConfiguration.java
@@ -1,6 +1,5 @@
 package io.github.jesseduarte.vendas;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -10,10 +9,15 @@ public class MinhaConfiguration {
 	//Se fosse configs de banco, seria dadosConfiguration
 	//Se fosse de seguranca, seria securityConfiguration
 	
+	
+	/*Eliminando esse codigo pq inseri ele no application.properties
+	 * 
+	 
 	@Bean(name = "applicationName") //Nome customizado. Será visto em qualquer contexto da aplicacao.
 	public String applicationName() {
 		return "Sistema de vendas";
 	}
+	*/
 }
 
 //Podemos chamar o bean de um objeto/metodo da classe Configuration. Usou eles, logo esta fzd injecao de dep.
