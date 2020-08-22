dc60325699ee7fb149df14aeb5fdf770b68fb862
jesseduarte <jesse__duarte@hotmail.com>
2020-08-22 12:41:10 -0300

    Aprendendo Configurations e beans

---
+++ vendas/src/main/java/io/github/jesseduarte/vendas/MinhaConfiguration.java
@@ -0,0 +1,20 @@
+package io.github.jesseduarte.vendas;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinhaConfiguration {
+	
+	//Determina configs gerais da aplicacao...
+	//Se fosse configs de banco, seria dadosConfiguration
+	//Se fosse de seguranca, seria securityConfiguration
+	
+	@Bean(name = "applicationName") //Nome customizado. Será visto em qualquer contexto da aplicacao.
+	public String applicationName() {
+		return "Sistema de vendas";
+	}
+}
+
+//Podemos chamar o bean de um objeto da classe Configuration
+//O Bean tambem pode ser uma conexao com o banco, um correio de email, etc.
